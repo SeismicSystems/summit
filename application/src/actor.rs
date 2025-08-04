da30dc6b79f4687ef8cd36ffbc30b8ca29fe858e
Matthias Wright <matthias.s.wright@gmail.com>
2025-08-04 17:58:55 +0800

    chore: pass genesis hash to mock engine client

--- application/src/actor.rs
+++ application/src/actor.rs
@@ -130,7 +130,6 @@ impl<R: Storage + Metrics + Clock + Spawner + governor::clock::Clock + Rng, C: E
                     let built = self.built_block.clone();
                     select! {
                             res = self.handle_proposal(parent, &mut syncer) => {
-
                                 match res {
                                     Ok(block) => {
                                         // store block
