889445940a762c790ceb5ccf35aa4a8ff320a274
daltoncoder <daltoncoder2@gmail.com>
2025-09-08 18:32:28 -0400

    bring back syncer/move to ed25519

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -2,10 +2,10 @@ pub mod args;
 pub mod config;
 pub mod engine;
 mod keys;
-#[cfg(test)]
-mod test_harness;
-#[cfg(test)]
-mod tests;
+//#[cfg(test)]
+//mod test_harness;
+//#[cfg(test)]
+//mod tests;
 
 #[cfg(feature = "prom")]
 mod prom;
