7d285f2c85ff3c2b15b3a308986435919810dd36
Matthias Wright <matthias.s.wright@gmail.com>
2025-10-15 03:47:01 +0700

    chore: add optional IP cli arg

--- node/src/args.rs
+++ node/src/args.rs
@@ -108,6 +108,9 @@ pub struct RunFlags {
         default_value_t = String::from("./example_genesis.toml")
     )]
     pub genesis_path: String,
+    /// IP address for this node (optional, will use genesis if not provided)
+    #[arg(long)]
+    pub ip: Option<String>,
 }
 
 impl Command {
@@ -240,16 +243,21 @@ impl Command {
             )
             .unwrap();
 
-            let our_ip = committee
-                .iter()
-                .find_map(|v| {
-                    if v.0 == config.signer.public_key() {
-                        Some(v.1)
-                    } else {
-                        None
-                    }
-                })
-                .expect("This node is not on the committee");
+            let our_ip = if let Some(ref ip_str) = flags.ip {
+                ip_str.parse::<SocketAddr>()
+                    .expect("Invalid IP address format")
+            } else {
+                committee
+                    .iter()
+                    .find_map(|v| {
+                        if v.0 == config.signer.public_key() {
+                            Some(v.1)
+                        } else {
+                            None
+                        }
+                    })
+                    .expect("This node is not on the committee")
+            };
 
             // Configure telemetry
             let log_level = Level::from_str(&flags.log_level).expect("Invalid log level");
@@ -434,16 +442,21 @@ pub fn run_node_with_runtime(
         )
         .unwrap();
 
-        let our_ip = committee
-            .iter()
-            .find_map(|v| {
-                if v.0 == config.signer.public_key() {
-                    Some(v.1)
-                } else {
-                    None
-                }
-            })
-            .expect("This node is not on the committee");
+        let our_ip = if let Some(ref ip_str) = flags.ip {
+            ip_str.parse::<SocketAddr>()
+                .expect("Invalid IP address format")
+        } else {
+            committee
+                .iter()
+                .find_map(|v| {
+                    if v.0 == config.signer.public_key() {
+                        Some(v.1)
+                    } else {
+                        None
+                    }
+                })
+                .expect("This node is not on the committee")
+        };
 
         // configure network
 
