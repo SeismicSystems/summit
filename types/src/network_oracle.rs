453f675a614f718d187e9a8a227df1cc944c88b2
Matthias Wright <matthias.s.wright@gmail.com>
2025-11-01 05:15:06 +0800

    feat: update network oracle

--- types/src/network_oracle.rs
+++ types/src/network_oracle.rs
@@ -1,23 +1,23 @@
 use commonware_cryptography::PublicKey;
-use commonware_p2p::authenticated::discovery::Oracle;
-use commonware_runtime::{Metrics, Spawner};
+use commonware_p2p::{Manager, authenticated::discovery::Oracle};
+use commonware_utils::set::Ordered;
 
 pub trait NetworkOracle<C: PublicKey>: Send + Sync + 'static {
     fn register(&mut self, index: u64, peers: Vec<C>) -> impl Future<Output = ()> + Send;
 }
 
-pub struct DiscoveryOracle<E: Spawner + Metrics, C: PublicKey> {
-    oracle: Oracle<E, C>,
+pub struct DiscoveryOracle<C: PublicKey> {
+    oracle: Oracle<C>,
 }
 
-impl<E: Spawner + Metrics, C: PublicKey> DiscoveryOracle<E, C> {
-    pub fn new(oracle: Oracle<E, C>) -> Self {
+impl<C: PublicKey> DiscoveryOracle<C> {
+    pub fn new(oracle: Oracle<C>) -> Self {
         Self { oracle }
     }
 }
 
-impl<E: Spawner + Metrics, C: PublicKey> NetworkOracle<C> for DiscoveryOracle<E, C> {
+impl<C: PublicKey> NetworkOracle<C> for DiscoveryOracle<C> {
     async fn register(&mut self, index: u64, peers: Vec<C>) {
-        self.oracle.register(index, peers).await;
+        self.oracle.update(index, Ordered::from(peers)).await;
     }
 }
