030cc0767fa4d0e3c65b12af2e21b2b8db92b552
Matthias Wright <matthias.s.wright@gmail.com>
2025-12-05 21:28:47 +0100

    chore: use forked simplex

--- node/src/tests/mod.rs
+++ node/src/tests/mod.rs
@@ -7,12 +7,12 @@ use commonware_macros::test_traced;
 use commonware_p2p::simulated::Link;
 use std::time::Duration;
 
-#[test_traced("INFO")]
+#[test_traced("DEBUG")]
 fn test_20_blocks_and_verify() {
     let link = Link {
         latency: Duration::from_millis(80),
         jitter: Duration::from_millis(10),
         success_rate: 0.98,
     };
-    run_until_height(10, 0, link.clone(), 20, true);
+    run_until_height(5, 0, link.clone(), 12, true);
 }
