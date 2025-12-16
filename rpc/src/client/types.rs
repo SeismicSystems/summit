4cd8d50accc50c81e39a9257df59ef510c68a16f
cdrappi <c@seismic.systems>
2025-12-16 15:23:09 -0500

    comment

--- rpc/src/client/types.rs
+++ rpc/src/client/types.rs
@@ -64,9 +64,9 @@ pub struct CheckpointInfoRes {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DepositTransactionResponse {
     pub node_pubkey: [u8; 32],
-    pub consensus_pubkey: Vec<u8>,
+    pub consensus_pubkey: Vec<u8>, // 48 bytes
     pub withdrawal_credentials: [u8; 32],
-    pub node_signature: Vec<u8>,
-    pub consensus_signature: Vec<u8>,
+    pub node_signature: Vec<u8>, // 48 bytes
+    pub consensus_signature: Vec<u8>, // 96 bytes
     pub deposit_data_root: [u8; 32],
 }
