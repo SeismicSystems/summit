fb5252227eb930880d0487676cea9751643b887f
Matthias Wright <matthias.s.wright@gmail.com>
2026-01-16 01:29:29 +0800

    fix: protocol params test

--- node/src/bin/protocol_params.rs
+++ node/src/bin/protocol_params.rs
@@ -182,7 +182,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                     executor.start(|node_context| async move {
                         let node_handle = node_context.clone().spawn(|ctx| async move {
-                            run_node_local(ctx, flags, None).await.unwrap();
+                            run_node_local(ctx, flags, None, None).await.unwrap();
                         });
 
                         // Wait for stop signal or node completion
@@ -374,7 +374,7 @@ fn get_node_flags(node: usize) -> RunFlags {
         #[cfg(any(feature = "base-bench", feature = "bench"))]
         bench_block_dir: None,
         checkpoint_path: None,
-        checkpoint_or_default: None,
+        checkpoint_or_default: false,
         ip: None,
     }
 }
