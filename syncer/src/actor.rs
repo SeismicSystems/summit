6349d402e95f5d000084e7c5f35414a0d2606c52
Matthias Wright <matthias.s.wright@gmail.com>
2026-01-28 02:58:20 +0800

    chore: add more logs to the syncer

--- syncer/src/actor.rs
+++ syncer/src/actor.rs
@@ -338,6 +338,7 @@ where
         self.last_processed_height = sync_height;
         self.last_processed_round = Round::new(Epoch::new(sync_epoch), View::new(sync_view));
         self.tip = sync_height;
+        info!(sync_height, sync_epoch, sync_view, "syncer initialized");
 
         // If we have a checkpoint last block meaning we loaded from checkpoint, finalize it to complete the checkpoint
         if let Some(last_block) = checkpoint_last_block {
@@ -1014,6 +1015,15 @@ where
 
         // Update metrics and send tip update to application
         if height > self.tip {
+            let gap = height - self.tip;
+            if gap > 1 {
+                debug!(
+                    previous_tip = self.tip,
+                    new_tip = height,
+                    gap,
+                    "tip advanced by multiple blocks (catch-up)"
+                );
+            }
             application.report(Update::Tip(height, commitment)).await;
             self.tip = height;
             #[cfg(feature = "prom")]
